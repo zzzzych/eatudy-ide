d051ce91f051408e429579a8a85dc6be4163a238
zzzzych <zzzzych@naver.com>
2023-02-23 09:44:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Eatudy IDE</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>어렵다 어려워어렵다 어려워</h2>
     </div>
   );
 }
