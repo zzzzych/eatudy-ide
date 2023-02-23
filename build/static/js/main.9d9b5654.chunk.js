ee904dc41ce2b72b3b35faf6e720cb3c3b3a9d61
zzzzych <zzzzych@naver.com>
2023-02-23 09:22:49 +0000

    Initial commit

---
+++ build/static/js/main.9d9b5654.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpeatudy-ide"]=this["webpackJsonpeatudy-ide"]||[]).push([[0],{12:function(e,t,c){},14:function(e,t,c){"use strict";c.r(t);var n=c(1),i=c(3),s=(c(12),c(0));function r(){return Object(s.jsxs)("div",{className:"App",children:[Object(s.jsx)("h1",{children:"Eatudy IDE"}),Object(s.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var d=document.getElementById("root");Object(i.createRoot)(d).render(Object(s.jsx)(n.StrictMode,{children:Object(s.jsx)(r,{})}))}},[[14,1,2]]]);
+//# sourceMappingURL=main.9d9b5654.chunk.js.map
